3004d0b4b7c91527b1b92b32d660860ca24747cb
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-02-19 16:21:17 +0000

    Modified Baseline
    Mostly just experimentation at this point - ignore the use of Octogarf

--- task_keeper/lib/main.dart
+++ task_keeper/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Task Keeper"),
+      home: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          title: Center(child: Text("Task Keeper")),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage("https://raw.githubusercontent.com/Hannah-Ashna/Garfield-Spigot-Plugin/main/Screenshots/octogarf.png"),
+          ),
+        ),
       ),
     ),
   );
