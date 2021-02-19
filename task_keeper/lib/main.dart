4d76ac3a5ea0ddf041558f08c2d5a73c789b020b
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-02-19 17:05:44 +0000

    Replaced test images with new logo

--- task_keeper/lib/main.dart
+++ task_keeper/lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: AssetImage("Images/TestImage.jpg"),
+            image: AssetImage("Images/TKLogo.png"),
           ),
         ),
       ),
